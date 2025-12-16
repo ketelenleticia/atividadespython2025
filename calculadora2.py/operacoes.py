abcf1f59755ff88a9c777a064fa512a3584995d2
kete <ketelen.lima10@gmail.com>
2025-12-16 06:29:54 -0300

    novo

---
+++ calculadora2.py/operacoes.py
@@ -0,0 +1,19 @@
+def soma(a,b):
+     return a +b
+
+
+def subtracao(a,b):
+    return a- b
+
+
+
+def multiplicacao(a,b):
+    return a*b
+
+
+
+def divisao(a,b):
+    if b == 0:
+        return("não pode dividir.")
+    else:
+        return(a/b)
