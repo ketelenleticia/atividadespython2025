4edc84d3843792faebcf971904e0fd002dbcc8e4
kete <ketelen.lima10@gmail.com>
2025-12-16 06:28:28 -0300

    novo
    Implement basic calculator operations with user input.

---
+++ calculadora.py/programa.py
@@ -0,0 +1,14 @@
+from operacoes import *
+c=0
+while True:
+    print(f"soma = {1}")
+    print(f"subtraçao = {2}")
+    print(f"multiplicaçao = {3}")
+    print(f"divisao = {4}")
+    print(F" finalizar = {0}")
+    n=int(input("digite o numero da operaçao: "))
+    if n == 1:
+        print(soma)
+        c+=1
+    else:
+        print("ok")    
